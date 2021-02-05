aa40fca456fbe053a306512ecbfceace6bbd6f55
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 01:04:10 +0300

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,13 +4,18 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - prints the last digit of an integer
+ * Return:
+ * zero if the result is correct
+ */
 int main(void)
 {
-	int n;
+	int n = -980;
 	int last_digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	/* n = rand() - RAND_MAX / 2; */
 	/* your code goes there */
 	char string[] = "Last digit of";
 
