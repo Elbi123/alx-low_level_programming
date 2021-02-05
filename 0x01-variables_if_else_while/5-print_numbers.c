0ce968dc842817e13d9913f2fa25b4fc424bf8e0
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 02:25:45 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers less than 10
+ * Return:  zero  if success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
