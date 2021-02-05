ed59e33a2ace2cfff290bc7780c793b32190c8a5
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 01:29:58 +0300

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -37,7 +37,7 @@ int main(void)
 	{
 		if (last_digit != 0)
 		{
-			printf("Last digit of %d is %d and is less that 6 and not 0\n",
+			printf("Last digit of %d is %d and is less than 6 and not 0\n",
 			       n, last_digit);
 		}
 	}
