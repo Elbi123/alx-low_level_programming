f3f0654820ff3529f6b75cf165caec3d2a25e613
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 00:23:47 +0300

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -13,25 +12,25 @@ int main(void)
 	/* your code goes there */
 	char string[] = "Last digit of";
 	int last_digit;
-	if(n > 0)
+	if (n > 0)
 	{
-	  last_digit = n % 10;
+		last_digit = n % 10;
 	}
-	if(n < 0)
+	if (n < 0)
 	{
-	  last_digit = n % 10;
+		last_digit = n % 10;
 	}
-	if(last_digit > 5)
+	if (last_digit > 5)
 	{
-	  printf("%s %d is %d is greater than 5\n", string, n, last_digit);
+		printf("%s %d is %d is greater than 5\n", string, n, last_digit);
 	}
-	if(last_digit == 0)
+	if (last_digit == 0)
 	{
-	  printf("%s %d is %d and is 0\n", string, n, last_digit);
+		printf("%s %d is %d and is 0\n", string, n, last_digit);
 	}
-	if(last_digit < 6 && last_digit != 0)
+	if (last_digit < 6 && last_digit != 0)
 	{
-	  printf("%s %d is %d and is less that 6 and not 0\n", string, n,
+		 printf("%s %d is %d and is less that 6 and not 0\n", string, n,
 	   last_digit);
 	}
 	return (0);
