315bdd81747c8cc0e9773500f34df6e4cc1c84e4
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 22:37:07 +0300

    print all combination

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all combination starting from 00 - 99
+ * Return:  zero  in success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar((i/10) + '0');
+		putchar((i%10) + '0');
+		if (i <= 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
