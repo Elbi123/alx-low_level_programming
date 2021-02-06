3bb6836781f1a56bd7464aa1350139dd8fffad0f
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 18:38:12 +0300

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,13 +5,16 @@
  */
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	
+	for (i = 0; i < 10; i++)
 	{
 		putchar((i) + '0');
 		if (i < 9)
 		{
 			putchar(',');
 		}
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
