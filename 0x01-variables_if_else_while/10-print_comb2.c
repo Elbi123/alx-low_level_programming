14b28168db6d24ab056034ed52c221e785961d71
ubuntu <elbetelworkineh59@gmail.com>
2021-02-06 22:38:55 +0300

    print all combinations

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,8 +9,8 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		putchar((i/10) + '0');
-		putchar((i%10) + '0');
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		if (i <= 98)
 		{
 			putchar(',');
