be29e8de77afc53d438821447a4c316f7d195e12
ubuntu <elbetelworkineh59@gmail.com>
2021-02-09 01:55:32 +0300

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * _islower - check the code for lowercase.
+ *@c: integer parameter
+ * Return: 1 if lower or 0 is upper
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	return (0);
+}
