7ef6b49b7c0826dec6ad5d21d6991ae0d628d060
ubuntu <elbetelworkineh59@gmail.com>
2021-02-09 02:43:17 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - absolute value function
+ *@num: integer value
+ * Return: o or num.
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (-(num));
+	}
+	return (0);
+}
