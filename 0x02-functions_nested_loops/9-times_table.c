8914ffd233f40368db0237d07d8b02a53771d9a2
ubuntu <elbetelworkineh59@gmail.com>
2021-02-09 16:14:39 +0300

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * times_table - prints multiplication table.
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j < 10)
+		{
+			if ((i * j) <= 9)
+			{
+				_putchar((i * j) + '0');
+			}
+			else if ((i * j) > 9)
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+			}
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
