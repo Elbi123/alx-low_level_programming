861b4773452e622addb0d2b05c62f6cf266d5fb7
ubuntu <elbetelworkineh59@gmail.com>
2021-02-09 16:20:47 +0300

    add two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - add two integers.
+ * @n1: first integer
+ * @n2: second integer
+ * Return: Always the sum.
+ */
+int add(int n1, int n2)
+{
+	int total = n1 + n2;
+
+	return (total);
+}
