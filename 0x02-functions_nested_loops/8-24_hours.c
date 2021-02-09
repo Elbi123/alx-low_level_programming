bcc570d109d074dfa118ae80fa58b6f0128dc77e
ubuntu <elbetelworkineh59@gmail.com>
2021-02-09 14:15:12 +0300

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
- * main - prints all combination starting from 00 - 99
- * Return:  zero  in success
+ * jack_bauer - prints all combination starting from 00 - 99
+ * Return:  prints 24 hrs in success
  */
 void jack_bauer(void)
 {
 	int i = 0;
+
 	while (i < 24)
 	{
 		int j = 0;
-		
+
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
@@ -20,6 +21,6 @@ void jack_bauer(void)
 			_putchar('\n');
 			j++;
 		}
-      		i++;
+		i++;
 	}
 }
