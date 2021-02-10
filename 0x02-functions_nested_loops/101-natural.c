669cf66ffde15d46d6e5aea8bbaa8e9e17bbaa52
ubuntu <elbetelworkineh59@gmail.com>
2021-02-10 19:53:30 +0300

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+ * main - prints sum
+ * Return: On success 0.
+ */
 int main(void)
 {
 	int n;
+
 	printf("Enter the number: ");
-	scanf("%d", n)
+	scanf("%d", &n);
+
 	int total = 0;
 	int i = 1;
+
 	while (i < n)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
