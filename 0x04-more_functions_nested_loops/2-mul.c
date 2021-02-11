878b8d4fd95fd97a9a44c3a481ce4aef9c37e7f3
ubuntu <elbetelworkineh59@gmail.com>
2021-02-11 14:44:49 +0300

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - check the code for Holberton School students.
+ * @a: first int val
+ * @b: second int val
+ * Return: the product of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
